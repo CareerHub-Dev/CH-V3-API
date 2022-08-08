5263888800792acdff59e20aed84633d96b451d8
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-08 18:57:26 +0300

    implement ICurrentAccountService

---
+++ WebUI/Services/CurrentAccountService.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Interfaces;
+
+namespace WebUI.Services;
+
+public class CurrentAccountService : ICurrentAccountService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentAccountService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public Guid? AccountId => _httpContextAccessor.HttpContext?.Items["AccountId"] as Guid?;
+}
