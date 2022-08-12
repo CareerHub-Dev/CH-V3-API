04ff1464f41c56457829762a2eab1d941264d22f
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-12 19:31:19 +0300

    add base controller

---
+++ WebUI/Controllers/ApiControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ApiControllerBase : ControllerBase
+{
+    public ISender Mediator => HttpContext.RequestServices.GetRequiredService<ISender>();
+}
