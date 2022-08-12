3b055c325acb95a20a2e3178f7640c8552aea8d8
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-12 16:54:40 +0300

    remove unnecessart interface

--- Application/Common/Interfaces/ISecurityService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Application.Common.Models.Authentication;
-
-namespace Application.Common.Interfaces;
-
-public interface ISecurityService
-{
-    Task<AuthenticateResult> AuthenticateAsync(string email, string password, string ipAddress, CancellationToken cancellationToken = default);
-}
