dfd40571816d9a654acb74e92ba10fc76ba5ba4f
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-13 21:04:01 +0300

    remove unnecessary service

--- Application/AccountManager/Models/AccountInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Application.AccountManager.Models;
-
-public class AccountInfo
-{
-    public Guid Id { get; set; }
-    public string Role { get; set; } = string.Empty;
-}
--- Application/Common/Interfaces/IAccountManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using Application.AccountManager.Models;
-
-namespace Application.Common.Interfaces;
-
-public interface IAccountManager
-{
-    Task<bool> IsInRoleAsync(Guid accountId, string role);
-    Task<AccountInfo?> GetAccountInfoAsync(Guid accountId);
-}
