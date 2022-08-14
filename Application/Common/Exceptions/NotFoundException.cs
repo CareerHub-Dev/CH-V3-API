811cb607b21f7d741999754f36d3e53f391c5c40
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-14 13:39:34 +0300

    key may be any type

--- Application/Common/Exceptions/NotFoundException.cs
+++ Application/Common/Exceptions/NotFoundException.cs
@@ -17,7 +17,7 @@ public class NotFoundException : Exception
     {
     }
 
-    public NotFoundException(string name, Guid key)
+    public NotFoundException(string name, object key)
         : base($"Entity \"{name}\" ({key}) was not found.")
     {
     }
