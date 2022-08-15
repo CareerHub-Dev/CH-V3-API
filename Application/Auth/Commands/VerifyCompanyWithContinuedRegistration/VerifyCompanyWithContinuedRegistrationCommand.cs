f1dfb0a8607a3cf417c5ec2ddd8985e4e720fd48
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-15 15:24:13 +0300

    add VerifyCompanyWithContinuedRegistrationCommand

---
+++ Application/Auth/Commands/VerifyCompanyWithContinuedRegistration/VerifyCompanyWithContinuedRegistrationCommand.cs
@@ -0,0 +1,47 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Auth.Commands.VerifyCompanyWithContinuedRegistration;
+
+public record VerifyCompanyWithContinuedRegistrationCommand : IRequest
+{
+    public string Token { get; set; } = string.Empty;
+
+    public string CompanyName { get; set; } = string.Empty;
+    public string CompanyMotto { get; set; } = string.Empty;
+    public string CompanyDescription { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+
+public class VerifyCompanyWithContinuedRegistrationCommandHandler : IRequestHandler<VerifyCompanyWithContinuedRegistrationCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public VerifyCompanyWithContinuedRegistrationCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(VerifyCompanyWithContinuedRegistrationCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Companies
+                .SingleOrDefaultAsync(x => x.VerificationToken == request.Token, cancellationToken);
+
+        if (entity == null)
+        {
+            throw new ArgumentException("Verification failed. Token does not exist");
+        }
+
+        entity.PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+        entity.VerificationToken = null;
+        entity.Verified = DateTime.UtcNow;
+        entity.CompanyName = request.CompanyName;
+        entity.CompanyMotto = request.CompanyMotto;
+        entity.CompanyDescription = request.CompanyDescription;
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
---
+++ Application/Auth/Commands/VerifyCompanyWithContinuedRegistration/VerifyCompanyWithContinuedRegistrationCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Entensions;
+using FluentValidation;
+
+namespace Application.Auth.Commands.VerifyCompanyWithContinuedRegistration;
+
+public class VerifyCompanyWithContinuedRegistrationCommandValidator : AbstractValidator<VerifyCompanyWithContinuedRegistrationCommand>
+{
+    public VerifyCompanyWithContinuedRegistrationCommandValidator()
+    {
+        RuleFor(x => x.Token).NotEmpty();
+        RuleFor(x => x.CompanyName).NotEmpty().MaximumLength(40);
+        RuleFor(x => x.CompanyMotto).NotEmpty().MaximumLength(40);
+        RuleFor(x => x.CompanyDescription).NotEmpty().MaximumLength(256);
+        RuleFor(x => x.Password).NotEmpty().Password();
+    }
+}
