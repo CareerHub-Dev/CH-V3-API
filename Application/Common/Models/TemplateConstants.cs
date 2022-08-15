665b397995babbc06cda11af69808db48aef6036
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-15 17:57:50 +0300

    change names and add new tamplate

--- Application/Common/Models/TemplateConstants.cs
+++ Application/Common/Models/TemplateConstants.cs
@@ -3,7 +3,8 @@
 public class TemplateConstants
 {
     public const string CompanyInvitationEmail = "company_invitation_email.html";
+    public const string AdminInvitationEmail = "admin_invitation_email.html";
     public const string PasswordResetEmail = "password_reset_email.html";
-    public const string VerificationEmail = "verification_email.html";
+    public const string VerifyStudentEmail = "verify_student_email.html";
     public const string AlreadyRegisteredEmail = "already_registered_email.html";
 }
