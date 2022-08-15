e66633e4e37709eaace7b0db8950186eb7675b54
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-15 15:22:21 +0300

    add FluentValidationExtensions

---
+++ Application/Common/Entensions/FluentValidationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Application.Common.Entensions;
+
+public static class FluentValidationExtensions
+{
+    private const string PasswordPattern = @"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9#?!@$%^&*-]).{8,32}$";
+
+    public static IRuleBuilderOptions<T, string> Password<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder
+            .Matches(PasswordPattern)
+            .WithMessage("The field '{PropertyName}' must match the regular expression '" + PasswordPattern + "'.");
+    }
+}
