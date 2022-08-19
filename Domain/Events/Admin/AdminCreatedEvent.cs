f0ea09c94b124396b2947cf83a2612e0fc023392
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-19 16:52:06 +0300

    add AdminCreatedEvent and remove unnecessary

---
+++ Domain/Events/Admin/AdminCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+
+namespace Domain.Events.Admin;
+
+public class AdminCreatedEvent : BaseEvent
+{
+    public AdminCreatedEvent(Entities.Admin company)
+    {
+        Admin = company;
+    }
+
+    public Entities.Admin Admin { get; }
+}
