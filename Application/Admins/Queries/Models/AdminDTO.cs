996163b9b2a9d1d56f8c7b34cab69272b30dfc2a
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-19 16:08:59 +0300

    add AdminsQueries

---
+++ Application/Admins/Queries/Models/AdminDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Admins.Queries.Models;
+
+public class AdminDTO
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public DateTime? Verified { get; set; }
+    public DateTime? PasswordReset { get; set; }
+}
