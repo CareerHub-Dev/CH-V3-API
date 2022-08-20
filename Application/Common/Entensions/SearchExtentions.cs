6f4b3e4850796502c37f3c3889938a188949e56e
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-21 00:21:29 +0300

    add SearchParameter

---
+++ Application/Common/Entensions/SearchExtentions.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Entensions;
+
+public static class SearchExtentions
+{
+    public static IQueryable<StudentGroup> Search(this IQueryable<StudentGroup> studentGroups, string searchTerm)
+    {
+
+        if (string.IsNullOrWhiteSpace(searchTerm))
+            return studentGroups;
+
+        var lowerCaseTerm = searchTerm.NormalizeName();
+
+        return studentGroups.Where(x => x.Name.Trim().ToLower().Contains(lowerCaseTerm));
+    }
+}
---
+++ Application/Common/Models/Search/SearchParameter.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Common.Models.Search;
+
+public class SearchParameter
+{
+    public string? SearchTerm { get; set; }
+}
