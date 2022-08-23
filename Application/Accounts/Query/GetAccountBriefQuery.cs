82d0763b8c192b2da018f04127ca74bae40987e4
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-08-23 17:45:32 +0300

    add GetAccountBriefQuery

---
+++ Application/Accounts/Query/GetAccountBriefQuery.cs
@@ -0,0 +1,35 @@
+﻿using Application.Accounts.Query.Models;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Accounts.Query;
+
+public record GetAccountBriefQuery(Guid AccountId) : IRequest<AccountBriefDTO>;
+
+public class GetStudentLogQueryHandler : IRequestHandler<GetAccountBriefQuery, AccountBriefDTO>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetStudentLogQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<AccountBriefDTO> Handle(GetAccountBriefQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Accounts
+            .AsNoTracking()
+            .Where(x => x.Id == request.AccountId)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (entity == null)
+        {
+            throw new NotFoundException(nameof(Account), request.AccountId);
+        }
+
+        return new AccountBriefDTO { Id = entity.Id, Email = entity.Email, Verified = entity.Verified };
+    }
+}
---
+++ Application/Accounts/Query/Models/AccountBriefDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Accounts.Query.Models;
+
+public class AccountBriefDTO
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public DateTime? Verified { get; set; }
+}
