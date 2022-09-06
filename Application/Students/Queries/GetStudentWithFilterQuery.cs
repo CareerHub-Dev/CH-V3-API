0e37467c28427f7b590297a93d263e69a1a7110a
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-09-06 16:44:23 +0300

    Create GetStudentWithFilterQuery.cs

---
+++ Application/Students/Queries/GetStudentWithFilterQuery.cs
@@ -0,0 +1,56 @@
+﻿using Application.Common.Entensions;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Models.StudentGroup;
+using Application.Students.Queries.Models;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Students.Queries;
+
+public record GetStudentWithFilterQuery : IRequest<StudentDTO>
+{
+    public Guid StudentId { get; init; }
+    public bool? IsVerified { get; init; }
+}
+
+public class GetStudentWithFilterQueryHandler
+    : IRequestHandler<GetStudentWithFilterQuery, StudentDTO>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetStudentWithFilterQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<StudentDTO> Handle(GetStudentWithFilterQuery request, CancellationToken cancellationToken)
+    {
+        var student = await _context.Students
+            .AsNoTracking()
+            .Where(x => x.Id == request.StudentId)
+            .Filter(IsVerified: request.IsVerified)
+            .Select(x => new StudentDTO
+            {
+                Id = x.Id,
+                Email = x.Email,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                PhotoId = x.PhotoId,
+                Phone = x.Phone,
+                BirthDate = x.BirthDate,
+                StudentGroup = new StudentGroupBriefDTO { Id = x.StudentGroup!.Id, Name = x.StudentGroup.Name },
+                Verified = x.Verified,
+                PasswordReset = x.PasswordReset
+            })
+            .FirstOrDefaultAsync();
+
+        if (student == null)
+        {
+            throw new NotFoundException(nameof(Student), request.StudentId);
+        }
+
+        return student;
+    }
+}
