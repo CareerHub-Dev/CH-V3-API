2a3438a2e2881e344e0edba588c1983ab4426c9d
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-09-07 13:12:10 +0300

    add GetEnums methods

---
+++ WebUI/Areas/Auth/EnumsController.cs
@@ -0,0 +1,57 @@
+﻿using Application.Enums.Queries.GetDegrees;
+using Application.Enums.Queries.GetExperienceLevels;
+using Application.Enums.Queries.GetJobTypes;
+using Application.Enums.Queries.GetLanguageLevels;
+using Application.Enums.Queries.GetTemplateLanguages;
+using Application.Enums.Queries.GetWorkFormats;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Authorize;
+
+namespace WebUI.Areas.Auth;
+
+[Authorize]
+[Route("api/Auth")]
+public class EnumsController : ApiControllerBase
+{
+    [HttpGet("Degrees")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DegreeDTO))]
+    public async Task<IActionResult> GetDegrees()
+    {
+        return Ok(await Mediator.Send(new GetDegreesQuery { }));
+    }
+
+    [HttpGet("ExperienceLevels")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ExperienceLevelDTO))]
+    public async Task<IActionResult> GetExperienceLevels()
+    {
+        return Ok(await Mediator.Send(new GetExperienceLevelsQuery { }));
+    }
+
+    [HttpGet("JobTypes")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(JobTypeDTO))]
+    public async Task<IActionResult> GetJobTypes()
+    {
+        return Ok(await Mediator.Send(new GetJobTypesQuery { }));
+    }
+
+    [HttpGet("LanguageLevels")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(JobTypeDTO))]
+    public async Task<IActionResult> GetLanguageLevels()
+    {
+        return Ok(await Mediator.Send(new GetLanguageLevelsQuery { }));
+    }
+
+    [HttpGet("TemplateLanguages")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(TemplateLanguageDTO))]
+    public async Task<IActionResult> GetTemplateLanguages()
+    {
+        return Ok(await Mediator.Send(new GetTemplateLanguagesQuery { }));
+    }
+
+    [HttpGet("WorkFormats")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(WorkFormatDTO))]
+    public async Task<IActionResult> GetWorkFormats()
+    {
+        return Ok(await Mediator.Send(new GetWorkFormatsQuery { }));
+    }
+}
