2e103d62f2804bfee7f51b2cf653ab22cefffc9b
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-09-19 12:10:12 +0300

    add DeleteCompanyLinkOfCompany

---
+++ Application/JobOffers/Commands/DeleteJobOfferOfCompany/DeleteJobOfferOfCompanyCommand.cs
@@ -0,0 +1,43 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.JobOffers.Commands.DeleteJobOfferOfCompany;
+
+public record DeleteJobOfferOfCompanyCommand(Guid JobOfferId, Guid CompanyId) : IRequest;
+
+public class DeleteJobOfferOfCompanyCommandHandler : IRequestHandler<DeleteJobOfferOfCompanyCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteJobOfferOfCompanyCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(DeleteJobOfferOfCompanyCommand request, CancellationToken cancellationToken)
+    {
+        if (!await _context.Companies
+            .AnyAsync(x => x.Id == request.CompanyId))
+        {
+            throw new NotFoundException(nameof(Company), request.CompanyId);
+        }
+
+        var jobOffer = await _context.JobOffers
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == request.JobOfferId && x.CompanyId == request.CompanyId);
+
+        if (jobOffer == null)
+        {
+            throw new NotFoundException(nameof(JobOffer), request.JobOfferId);
+        }
+
+        _context.JobOffers.Remove(jobOffer);
+
+        await _context.SaveChangesAsync();
+
+        return Unit.Value;
+    }
+}
