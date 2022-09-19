7b7e2849cca224b894c2fa049028274dd04abbfd
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-09-19 12:45:57 +0300

    add jobOffer subscription methods

---
+++ Application/JobOffers/Commands/VerifiedStudentSubscribeToActiveJobOffer/VerifiedStudentSubscribeToActiveJobOfferCommand.cs
@@ -0,0 +1,57 @@
+﻿using Application.Common.Entensions;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.JobOffers.Commands.VerifiedStudentSubscribeToActiveJobOffer;
+
+public record VerifiedStudentSubscribeToActiveJobOfferCommand : IRequest
+{
+    public Guid StudentId { get; init; }
+    public Guid JobOfferId { get; init; }
+}
+
+public class VerifiedStudentSubscribeToActiveJobOfferCommandHandler : IRequestHandler<VerifiedStudentSubscribeToActiveJobOfferCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public VerifiedStudentSubscribeToActiveJobOfferCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(VerifiedStudentSubscribeToActiveJobOfferCommand request, CancellationToken cancellationToken)
+    {
+        var student = await _context.Students
+            .Filter(isVerified: true)
+            .FirstOrDefaultAsync(x => x.Id == request.StudentId);
+
+        if (student == null)
+        {
+            throw new NotFoundException(nameof(Student), request.StudentId);
+        }
+
+        var joboffer = await _context.JobOffers
+            .Include(x => x.SubscribedStudents)
+            .Filter(isActive: true)
+            .FirstOrDefaultAsync(x => x.Id == request.JobOfferId);
+
+        if (joboffer == null)
+        {
+            throw new NotFoundException(nameof(JobOffer), request.JobOfferId);
+        }
+
+        if (joboffer.SubscribedStudents.Any(x => x.Id == request.StudentId))
+        {
+            return Unit.Value;
+        }
+
+        joboffer.SubscribedStudents.Add(student);
+
+        await _context.SaveChangesAsync();
+
+        return Unit.Value;
+    }
+}
---
+++ Application/JobOffers/Commands/VerifiedStudentUnubscribeFromActiveJobOffer/VerifiedStudentUnubscribeFromActiveJobOfferCommand.cs
@@ -0,0 +1,59 @@
+﻿using Application.Common.Entensions;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.JobOffers.Commands.VerifiedStudentUnubscribeFromActiveJobOffer;
+
+public record VerifiedStudentUnubscribeFromActiveJobOfferCommand : IRequest
+{
+    public Guid StudentId { get; init; }
+    public Guid JobOfferId { get; init; }
+}
+
+public class VerifiedStudentUnubscribeFromActiveJobOfferCommandHandler : IRequestHandler<VerifiedStudentUnubscribeFromActiveJobOfferCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public VerifiedStudentUnubscribeFromActiveJobOfferCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(VerifiedStudentUnubscribeFromActiveJobOfferCommand request, CancellationToken cancellationToken)
+    {
+        var student = await _context.Students
+            .Filter(isVerified: true)
+            .FirstOrDefaultAsync(x => x.Id == request.StudentId);
+
+        if (student == null)
+        {
+            throw new NotFoundException(nameof(Student), request.StudentId);
+        }
+
+        var company = await _context.JobOffers
+            .Include(x => x.SubscribedStudents)
+            .Filter(isActive: true)
+            .FirstOrDefaultAsync(x => x.Id == request.JobOfferId);
+
+        if (company == null)
+        {
+            throw new NotFoundException(nameof(JobOffer), request.JobOfferId);
+        }
+
+        var subscribedStudent = company.SubscribedStudents.FirstOrDefault(x => x.Id == request.StudentId);
+
+        if (subscribedStudent == null)
+        {
+            return Unit.Value;
+        }
+
+        company.SubscribedStudents.Remove(subscribedStudent);
+
+        await _context.SaveChangesAsync();
+
+        return Unit.Value;
+    }
+}
