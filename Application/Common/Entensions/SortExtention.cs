dcb391aa84aa9c9293e2b90f493a89b973d8396c
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-09-29 13:35:06 +0300

    add Sort Extention

---
+++ Application/Common/Entensions/SortExtention.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+
+namespace Application.Common.Entensions;
+
+public static class SortExtention
+{
+    public static IQueryable<T> OrderByExpression<T>(this IQueryable<T> query, string orderByExpression)
+    {
+        if (string.IsNullOrWhiteSpace(orderByExpression))
+            return query;
+
+        string propertyName, orderByMethod;
+        string[] strs = orderByExpression.Split(' ');
+        propertyName = strs[0];
+
+        if (strs.Length == 1)
+            orderByMethod = "OrderBy";
+        else
+            orderByMethod = strs[1].Equals("DESC", StringComparison.OrdinalIgnoreCase) ? "OrderByDescending" : "OrderBy";
+
+        ParameterExpression pe = Expression.Parameter(query.ElementType);
+        MemberExpression me = Expression.Property(pe, propertyName);
+
+        MethodCallExpression orderByCall = Expression.Call(typeof(Queryable), orderByMethod, new Type[] { query.ElementType, me.Type }, query.Expression
+            , Expression.Quote(Expression.Lambda(me, pe)));
+
+        return (IQueryable<T>)query.Provider.CreateQuery(orderByCall);
+    }
+}
