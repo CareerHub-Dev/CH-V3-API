8d1b768db050801cabe218fc4ceead4ca977e2d8
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-10-04 00:09:20 +0300

    rename classes

--- Application/JobOffers/Commands/VerifiedActiveStudentSubscribeToActiveJobOfferWithVerifiedActiveCompany/VerifiedActiveStudentSubscribeToActiveJobOfferWithVerifiedActiveCompanyCommand.cs
+++ Application/JobOffers/Commands/VerifiedActiveStudentSubscribeToActiveJobOfferWithVerifiedActiveCompany/VerifiedActiveStudentSubscribeToActiveJobOfferWithVerifiedActiveCompanyCommand.cs
@@ -2,30 +2,35 @@
 using Application.Common.Exceptions;
 using Application.Common.Interfaces;
 using Domain.Entities;
+using Domain.Enums;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-namespace Application.JobOffers.Commands.VerifiedStudentSubscribeToActiveJobOffer;
+namespace Application.JobOffers.Commands.VerifiedActiveStudentSubscribeToActiveJobOfferWithVerifiedActiveCompany;
 
-public record VerifiedStudentSubscribeToActiveJobOfferCommand : IRequest
+public record VerifiedActiveStudentSubscribeToActiveJobOfferWithVerifiedActiveCompanyCommand : IRequest
 {
     public Guid StudentId { get; init; }
     public Guid JobOfferId { get; init; }
 }
 
-public class VerifiedStudentSubscribeToActiveJobOfferCommandHandler : IRequestHandler<VerifiedStudentSubscribeToActiveJobOfferCommand>
+public class VerifiedActiveStudentSubscribeToActiveJobOfferWithVerifiedActiveCompanyCommandHandler
+    : IRequestHandler<VerifiedActiveStudentSubscribeToActiveJobOfferWithVerifiedActiveCompanyCommand>
 {
     private readonly IApplicationDbContext _context;
 
-    public VerifiedStudentSubscribeToActiveJobOfferCommandHandler(IApplicationDbContext context)
+    public VerifiedActiveStudentSubscribeToActiveJobOfferWithVerifiedActiveCompanyCommandHandler(IApplicationDbContext context)
     {
         _context = context;
     }
 
-    public async Task<Unit> Handle(VerifiedStudentSubscribeToActiveJobOfferCommand request, CancellationToken cancellationToken)
+    public async Task<Unit> Handle(VerifiedActiveStudentSubscribeToActiveJobOfferWithVerifiedActiveCompanyCommand request, CancellationToken cancellationToken)
     {
         var student = await _context.Students
-            .Filter(isVerified: true)
+            .Filter(
+                isVerified: true, 
+                activationStatus: ActivationStatus.Active
+            )
             .FirstOrDefaultAsync(x => x.Id == request.StudentId);
 
         if (student == null)
@@ -35,7 +40,11 @@ public class VerifiedStudentSubscribeToActiveJobOfferCommandHandler : IRequestHa
 
         var joboffer = await _context.JobOffers
             .Include(x => x.SubscribedStudents)
-            .Filter(isActive: true)
+            .Filter(
+                isActive: true,
+                isCompanyVerified: true,
+                companyActivationStatus: ActivationStatus.Active
+            )
             .FirstOrDefaultAsync(x => x.Id == request.JobOfferId);
 
         if (joboffer == null)
