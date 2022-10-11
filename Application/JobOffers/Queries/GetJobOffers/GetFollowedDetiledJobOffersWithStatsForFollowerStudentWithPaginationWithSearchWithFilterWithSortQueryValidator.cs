e454c84974b40bd78ff2cf0d89041bc6665a78b0
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-10-11 22:05:39 +0300

    add validator

---
+++ Application/JobOffers/Queries/GetJobOffers/GetFollowedDetiledJobOffersWithStatsForFollowerStudentWithPaginationWithSearchWithFilterWithSortQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Application.JobOffers.Queries.GetJobOffers;
+
+public class GetFollowedDetiledJobOffersWithStatsForFollowerStudentWithPaginationWithSearchWithFilterWithSortQueryValidator
+    : AbstractValidator<GetFollowedDetiledJobOffersWithStatsForFollowerStudentWithPaginationWithSearchWithFilterWithSortQuery>
+{
+	public GetFollowedDetiledJobOffersWithStatsForFollowerStudentWithPaginationWithSearchWithFilterWithSortQueryValidator()
+	{
+        RuleFor(x => x.PageNumber)
+            .GreaterThanOrEqualTo(1);
+
+        RuleFor(x => x.PageSize)
+            .GreaterThanOrEqualTo(1)
+            .LessThanOrEqualTo(50);
+    }
+}
