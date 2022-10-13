5b1ad707a1d89af369646725f2cea6018aac48e3
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-10-13 13:03:51 +0300

    add GetDetiledJobOfferSubscriptionsWithStatsOfStudentOwnerWithPaginationWithSearchWithFilterWithSortQuery

---
+++ Application/JobOffers/Queries/GetJobOfferSubscriptionsOfStudent/GetDetiledJobOfferSubscriptionsWithStatsOfStudentOwnerWithPaginationWithSearchWithFilterWithSortQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Application.JobOffers.Queries.GetJobOfferSubscriptionsOfStudent;
+
+public class GetDetiledJobOfferSubscriptionsWithStatsOfStudentOwnerWithPaginationWithSearchWithFilterWithSortQueryValidator
+	: AbstractValidator<GetDetiledJobOfferSubscriptionsWithStatsOfStudentOwnerWithPaginationWithSearchWithFilterWithSortQuery>
+{
+	public GetDetiledJobOfferSubscriptionsWithStatsOfStudentOwnerWithPaginationWithSearchWithFilterWithSortQueryValidator()
+	{
+        RuleFor(x => x.PageNumber)
+            .GreaterThanOrEqualTo(1);
+
+        RuleFor(x => x.PageSize)
+            .GreaterThanOrEqualTo(1)
+            .LessThanOrEqualTo(50);
+    }
+}
