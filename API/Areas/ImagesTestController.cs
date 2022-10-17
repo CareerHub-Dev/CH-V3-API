3f8aca8d8873e73d0fda65d33395e05969dae166
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-10-17 14:42:01 +0300

    Test methods

---
+++ API/Areas/ImagesTestController.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Areas;
+
+[Route("api/[controller]")]
+public class ImagesTestController : ApiControllerBase
+{
+    private readonly IPathService _pathService;
+
+    public ImagesTestController(IPathService pathService)
+    {
+        _pathService = pathService;
+    }
+
+    [HttpDelete("delete/{fileName}")]
+    public IActionResult Delete(string fileName)
+    {
+        FileService fileService = new FileService();
+
+        fileService.RemoveFile(_pathService.GetImagePath(fileName));
+
+        return Ok();
+    }
+
+    [HttpPost("create")]
+    public async Task<IActionResult> Create(IFormFile file)
+    {
+        FileService fileService = new FileService();
+
+        await fileService.MoveFileAsync(file, _pathService.GetImagesPath(), file.FileName);
+
+        return Ok();
+    }
+}
