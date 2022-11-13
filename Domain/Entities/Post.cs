5943497d32a50c7c5d2558c7775f4822f05fc538
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-11-13 12:20:02 +0200

    add images in post

--- Domain/Entities/Post.cs
+++ Domain/Entities/Post.cs
@@ -5,6 +5,7 @@ namespace Domain.Entities;
 public class Post : BaseEntity
 {
     public string Text { get; set; } = string.Empty;
+    public List<string> Images { get; set; } = new List<string>();
     public DateTime CreatedDate { get; set; }
 
     public Guid AccoundId { get; set; }
--- Infrastructure/Persistence/Migrations/20221113101549_AddImagesInPost.Designer.cs
+++ Infrastructure/Persistence/Migrations/20221113101549_AddImagesInPost.Designer.cs
@@ -6,6 +6,7 @@ using Domain.Enums;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -14,9 +15,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221113101549_AddImagesInPost")]
+    partial class AddImagesInPost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
