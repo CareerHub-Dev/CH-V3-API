21584fb2d96e8fa5d14dc403529bb61704203d00
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-04-24 13:28:33 +0300

    add RemoveAuditionFromJobPosition

---
+++ Infrastructure/Persistence/Migrations/20230424102800_RemoveAuditionFromJobPosition.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAuditionFromJobPosition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "JobPositions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "JobPositions");
+
+            migrationBuilder.DropColumn(
+                name: "LastModified",
+                table: "JobPositions");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "JobPositions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "JobPositions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                table: "JobPositions",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModified",
+                table: "JobPositions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastModifiedBy",
+                table: "JobPositions",
+                type: "uuid",
+                nullable: true);
+        }
+    }
+}
