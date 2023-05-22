f60d045658ba1726c61906342464355f8cb001aa
hyperborean_one <spburtsev@proton.me>
2023-05-22 20:49:58 +0300

    ForeignLanguage#ToString added

--- Domain/Entities/ForeignLanguage.cs
+++ Domain/Entities/ForeignLanguage.cs
@@ -1,9 +1,13 @@
 ﻿using Domain.Enums;
-
 namespace Domain.Entities;
 
 public class ForeignLanguage
 {
     public string Name { get; set; } = string.Empty;
     public LanguageLevel LanguageLevel { get; set; }
+
+    public override string ToString()
+    {
+        return Name + " \u2013 " + LanguageLevel;
+    }
 }
