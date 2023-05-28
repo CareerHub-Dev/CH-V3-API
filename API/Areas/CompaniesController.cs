ad90285ef1192ad22f8dac1197497cadfe7fbb4b
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-05-28 16:43:27 +0300

    add get companies

---
+++ API/Areas/CompaniesController.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.DTO.Companies;
+using Application.Companies.Queries.GetBriefCompanies;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API.Areas;
+
+[Route("api/[controller]")]
+public class CompaniesController : ApiControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<BriefCompanyDTO>))]
+    public async Task<IActionResult> GetCompanies(
+        [FromQuery] string? order,
+        [FromQuery] string? search,
+        [FromQuery] int pageNumber = 1,
+        [FromQuery] int pageSize = 10)
+    {
+        var result = await Sender.Send(new GetBriefCompaniesWithPagingQuery
+        {
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+
+            SearchTerm = search ?? string.Empty,
+
+            IsCompanyMustBeVerified = true,
+
+            OrderByExpression = order ?? "Name",
+        });
+
+        Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(result.MetaData));
+
+        return Ok(result);
+    }
+}
---
+++ Application/Companies/Queries/GetBriefCompanies/GetBriefCompaniesWithPagingQuery.cs
@@ -0,0 +1,47 @@
+﻿using Application.Common.DTO.Companies;
+using Application.Common.Entensions;
+using Application.Common.Interfaces;
+using Application.Common.Models.Pagination;
+using Application.Companies.Queries.Models;
+using MediatR;
+
+namespace Application.Companies.Queries.GetBriefCompanies;
+
+public record GetBriefCompaniesWithPagingQuery
+    : IRequest<PaginatedList<BriefCompanyDTO>>
+{
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+
+    public string SearchTerm { get; init; } = string.Empty;
+
+    public bool? IsCompanyMustBeVerified { get; init; }
+
+    public string OrderByExpression { get; init; } = string.Empty;
+}
+
+public class GetBriefCompaniesWithPagingQueryHandler
+    : IRequestHandler<GetBriefCompaniesWithPagingQuery, PaginatedList<BriefCompanyDTO>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetBriefCompaniesWithPagingQueryHandler(
+        IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<PaginatedList<BriefCompanyDTO>> Handle(
+        GetBriefCompaniesWithPagingQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await _context.Companies
+            .Filter(
+                isVerified: request.IsCompanyMustBeVerified
+            )
+            .Search(request.SearchTerm)
+            .MapToBriefCompanyDTO()
+            .OrderByExpression(request.OrderByExpression)
+            .ToPagedListAsync(request.PageNumber, request.PageSize);
+    }
+}
